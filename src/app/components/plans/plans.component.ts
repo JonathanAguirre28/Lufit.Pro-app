b382cefe8163fa8c0a91166eef1bef50f0c1899b
JonathanAguirre <jonathan621924@gmail.com>
2023-11-11 11:23:50 -0300

    add cards

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./plans.component.scss']
 })
 export class PlansComponent {
+  imagen: string = '';
+  imageMobile: string = '';
+  titulo: string = '';
+  span: string = '';
+  subtitulo: string = '';
+  text: string = '';
+
+  cards = [
+    {
+      imagen: "assets/image/imagePlansOne.png",
+      imageMobile:"assets/image/imageMobilePlansOne.png",
+      titulo: "ENTRENAMIENTO PERSONALIZADO",
+      span: "(tucumán)",
+      subtitulo: 'Plan por 30 dias adaptado a tus necesidades y preferencias.',
+      text: 'Acompañamiento presencial en gimnasio.',
+    },
+    {
+      imagen:"assets/image/imagePlansTwo.png",
+      imageMobile:"assets/image/imageMobilePlansTwo.png",
+      titulo: "ENTRENAMIENTO PERSONALIZADO ONLINE",
+      span: "EN GIMNASIO",
+      subtitulo: "Entrena conmigo donde estés. Plan por 30 dias adaptadp a tus necesidades.",
+      text: "Seguimiento integral online."
+    },
+    {
+      imagen:"assets/image/imagePlansThree.png",
+      imageMobile:"assets/image/imageMobilePlansThree.png",
+      titulo: "ENTRENAMIENTO PERSONALIZADO ONLINE",
+      span: "EN TU CASA",
+      subtitulo: "Plan por 30 dias para entrenar conmigo con los materiales que tengas en casa.",
+      text: "Seguimiento integral online."
+    }
+  ]
 
 }
