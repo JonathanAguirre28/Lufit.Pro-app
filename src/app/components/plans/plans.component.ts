baed85a7e5c5a3fa48d06a39660692fda69f7640
Franco Osores <franco_osores@hotmail.com>
2023-11-24 21:35:11 -0300

    Fix on plans

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
-  styleUrls: ['./plans.component.scss']
+  styleUrls: ['./plans.component.scss'],
 })
 export class PlansComponent {
   imagen: string = '';
@@ -15,29 +15,30 @@ export class PlansComponent {
 
   cards = [
     {
-      imagen: "assets/image/imagePlansOne.png",
-      imageMobile:"assets/image/imageMobilePlansOne.png",
-      titulo: "ENTRENAMIENTO PERSONALIZADO",
-      span: "(tucumán)",
-      subtitulo: 'Plan por 30 dias adaptado a tus necesidades y preferencias.',
+      imagen: 'assets/image/imagePlansOne.png',
+      imageMobile: 'assets/image/imagePlansOne.png',
+      titulo: 'ENTRENAMIENTO PERSONALIZADO',
+      span: '(Tucumán)',
+      subtitulo: 'Plan por 30 días adaptado a tus necesidades y preferencias.',
       text: 'Acompañamiento presencial en gimnasio.',
     },
     {
-      imagen:"assets/image/imagePlansTwo.png",
-      imageMobile:"assets/image/imageMobilePlansTwo.png",
-      titulo: "ENTRENAMIENTO PERSONALIZADO ONLINE",
-      span: "EN GIMNASIO",
-      subtitulo: "Entrena conmigo donde estés. Plan por 30 dias adaptadp a tus necesidades.",
-      text: "Seguimiento integral online."
+      imagen: 'assets/image/imagePlansTwo.png',
+      imageMobile: 'assets/image/imagePlansTwo.png',
+      titulo: 'ENTRENAMIENTO PERSONALIZADO ONLINE',
+      span: 'EN GIMNASIO',
+      subtitulo:
+        'Entrena conmigo donde estés. Plan por 30 días adaptado a tus necesidades.',
+      text: 'Seguimiento integral online.',
     },
     {
-      imagen:"assets/image/imagePlansThree.png",
-      imageMobile:"assets/image/imageMobilePlansThree.png",
-      titulo: "ENTRENAMIENTO PERSONALIZADO ONLINE",
-      span: "EN TU CASA",
-      subtitulo: "Plan por 30 dias para entrenar conmigo con los materiales que tengas en casa.",
-      text: "Seguimiento integral online."
-    }
-  ]
-
+      imagen: 'assets/image/imagePlansThree.png',
+      imageMobile: 'assets/image/imagePlansThree.png',
+      titulo: 'ENTRENAMIENTO PERSONALIZADO ONLINE',
+      span: 'EN TU CASA',
+      subtitulo:
+        'Plan por 30 dias para entrenar conmigo con los materiales que tengas en casa.',
+      text: 'Seguimiento integral online.',
+    },
+  ];
 }
