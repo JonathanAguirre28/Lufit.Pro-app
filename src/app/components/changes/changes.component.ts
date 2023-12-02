d8975f3a692161f2a2ebd57067b999c1883125f1
Franco Osores <franco_osores@hotmail.com>
2023-12-02 14:45:43 -0300

    Quixk fixes

--- src/app/components/changes/changes.component.ts
+++ src/app/components/changes/changes.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-changes',
   templateUrl: './changes.component.html',
-  styleUrls: ['./changes.component.scss']
+  styleUrls: ['./changes.component.scss'],
 })
 export class ChangesComponent {
-
+  constructor(private router: Router) {}
+  navigate() {
+    window.open('https://wa.me/5493813043142?text=Hola%20Luciana,%20¡Quiero%20comenzar%20a%20mejorar%20mi%20vida!', '_blank');
+  }
 }
