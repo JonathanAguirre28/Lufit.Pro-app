3733d19d09479712499a7fff98004730df6fce7f
Franco Osores <franco_osores@hotmail.com>
2024-11-30 20:11:37 -0300

    Finished plans section

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -15,34 +15,30 @@ export class PlansComponent {
 
   cards = [
     {
-      imagen: 'assets/image/imagePlansOne.png',
-      imageMobile: 'assets/image/imagePlansOne.png',
+      imagen: 'assets/card-images/card1.png',
       titulo: 'ENTRENAMIENTO PERSONALIZADO',
-      span: '(Tucumán)',
       subtitulo: 'Plan por 30 días adaptado a tus necesidades y preferencias.',
-      text: 'Acompañamiento presencial en gimnasio.',
+      text: 'Plan por 30 días adaptado a tus necesidades y preferencias. Acompañamiento presencial en gimnasio.',
       message:
         'https://wa.me/5493813043142?text=Hola Luciana, me interesa preguntar por "Plan por 30 días - ENTRENAMIENTO PERSONALIZADO"',
     },
     {
-      imagen: 'assets/image/imagePlansTwo.png',
-      imageMobile: 'assets/image/imagePlansTwo.png',
+      imagen: 'assets/card-images/card2.png',
       titulo: 'ENTRENAMIENTO PERSONALIZADO ONLINE',
       span: 'EN GIMNASIO',
       subtitulo:
         'Entrena conmigo donde estés. Plan bimestral adaptado a tus necesidades.',
-      text: 'Seguimiento integral online.',
+      text: 'Entrena conmigo donde estés. Plan bimestral adaptado a tus necesidades. Seguimiento integral online.',
       message:
         'https://wa.me/5493813043142?text=Hola Luciana, me interesa preguntar por "Plan bimestral ONLINE- ENTRENAMIENTO EN GIMNASIO"',
     },
     {
-      imagen: 'assets/image/imagePlansThree.png',
-      imageMobile: 'assets/image/imagePlansThree.png',
+      imagen: 'assets/card-images/card3.png',
       titulo: 'ENTRENAMIENTO PERSONALIZADO ONLINE',
       span: 'EN TU CASA',
       subtitulo:
         'Plan bimestral para entrenar conmigo con los materiales que tengas en casa.',
-      text: 'Seguimiento integral online.',
+      text: 'Plan bimestral para entrenar  conmigo con los materiales que tengas en casa. Seguimiento integral online.',
       message:
         'https://wa.me/5493813043142?text=Hola Luciana, me interesa preguntar por "Plan bimestral ONLINE- ENTRENAMIENTO EN CASA"',
     },
